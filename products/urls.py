01aabdf7d63638f7e1fdb01efdd2f1b306aca450
Kirillmlk <kostakov.kiirill@yandex.by>
2023-03-29 13:59:50 +0300

    finished project

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,9 @@ from products.views import products
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index')
+    path('', products, name='index'),
+    path('category/<int:category_id>', products, name='category'),
+    path('page/<int:page_number>', products, name='paginator'),
 ]
 
 
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from products.models import ProductCategory, Product
 
+from django.core.paginator import Paginator
+
 
 def index(request):
     context = {
@@ -9,10 +11,14 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    context = {
-        'title': 'Store - Каталог',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
+
+    context = {'title': 'Store - Каталог',
+               'categories': ProductCategory.objects.all(),
+               'products': products_paginator,
+               }
     return render(request, 'products/products.html', context)
